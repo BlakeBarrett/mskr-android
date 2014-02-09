1325f3ad70349c3df37c4fe6d91d15f81e61237b
Blake Barrett <blake@blakebarrett.net>
2014-02-09 11:38:41 -0800

    This is the desired masking behavior.

--- src/com/blakebarrett/mskr/MaskedBitmap.java
+++ src/com/blakebarrett/mskr/MaskedBitmap.java
@@ -5,6 +5,7 @@ import java.io.FileOutputStream;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.Config;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.PorterDuff;
@@ -75,23 +76,55 @@ public class MaskedBitmap {
 
 	public static Bitmap draw(final Bitmap source, final Bitmap mask) {
 
-		Bitmap scaledMask = makeItSquare(MAXIMUM_IMGE_SIZE, mask,
+		final Bitmap scaledMask = makeItSquare(MAXIMUM_IMGE_SIZE, mask,
 				SquareMode.LETTERBOX);
 
-		Bitmap maskedBitmap = makeItSquare(MAXIMUM_IMGE_SIZE, source,
+		final Bitmap croppedSource = makeItSquare(MAXIMUM_IMGE_SIZE, source,
 				SquareMode.CROP);
 
-		final Canvas canvas = new Canvas(maskedBitmap);
+		return merge(createBackgroundLayer(croppedSource),
+				applyMaskToBitmap(croppedSource, scaledMask));
+	}
+
+	private static Bitmap merge(final Bitmap... bitmaps) {
+		final Bitmap merged = Bitmap.createBitmap(bitmaps[0].getWidth(),
+				bitmaps[0].getHeight(), bitmaps[0].getConfig());
+		final Canvas canvas = new Canvas(merged);
+		for (int i = 0; i < bitmaps.length; i++) {
+			canvas.drawBitmap(bitmaps[i], new Matrix(), null);
+		}
+		return merged;
+	}
+
+	private static Bitmap createBackgroundLayer(final Bitmap source) {
+		final Bitmap temp = Bitmap.createBitmap(source.getWidth(),
+				source.getHeight(), source.getConfig());
+
+		final Canvas canvas = new Canvas();
+		canvas.drawColor(Color.WHITE);
+		canvas.setBitmap(temp);
+
+		final Paint paint = new Paint(Paint.DITHER_FLAG);
+		paint.setAlpha(128);
+		canvas.drawBitmap(source, new Matrix(), paint);
+		paint.setXfermode(null);
+
+		return temp;
+	}
+
+	private static Bitmap applyMaskToBitmap(final Bitmap source,
+			final Bitmap mask) {
+
+		final Canvas canvas = new Canvas(source);
 
 		final Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
 		paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_IN));
-		canvas.drawBitmap(scaledMask, 0, 0, paint);
+		canvas.drawBitmap(mask, 0, 0, paint);
 		paint.setXfermode(null);
 
-		scaledMask.recycle();
 		mask.recycle();
 
-		return maskedBitmap;
+		return source;
 	}
 
 	public static void save(final String filename, final Bitmap image) {
